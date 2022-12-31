d19bd4a6ea90339f4419e951237b79cc0ec544a0
Julian Churchill <chewielouie03@gmail.com>
2022-12-31 12:28:56 +0000

    Completed day 2 part 2

--- day2/tests/Day2Tests.cs
+++ day2/tests/Day2Tests.cs
@@ -14,47 +14,83 @@ public class Day2Tests
         CalculateTotalScore("").Should().Be(0);
     }
 
-    [TestCase(RockThem, RockUs, 4)]
-    [TestCase(PaperThem, PaperUs, 5)]
-    [TestCase(ScissorsThem, ScissorsUs, 6)]
+    // [TestCase(RockThem, LoseIndicator, 4)]
+    // [TestCase(PaperThem, DrawIndicator, 5)]
+    // [TestCase(ScissorsThem, WinIndicator, 6)]
+    // public void DrawsAreTheValueOfTheItemPlayedPlus3(char theirPlay, char ourPlay, int expectedScore)
+    // {
+    //     CalculateTotalScore($"{theirPlay} {ourPlay}").Should().Be(expectedScore);
+    // }
+
+    // [TestCase(RockThem, WinIndicator, 3)]
+    // [TestCase(PaperThem, LoseIndicator, 1)]
+    // [TestCase(ScissorsThem, DrawIndicator, 2)]
+    // public void ALossIsWorthTheItemPlayed(char theirPlay, char ourPlay, int expectedScore)
+    // {
+    //     CalculateTotalScore($"{theirPlay} {ourPlay}").Should().Be(expectedScore);
+    // }
+
+    // [TestCase(RockThem, DrawIndicator, 8)]
+    // [TestCase(PaperThem, WinIndicator, 9)]
+    // [TestCase(ScissorsThem, LoseIndicator, 7)]
+    // public void AWinIsWorthTheItemPlayedPlus6(char theirPlay, char ourPlay, int expectedScore)
+    // {
+    //     CalculateTotalScore($"{theirPlay} {ourPlay}").Should().Be(expectedScore);
+    // }
+
+    // [Test]
+    // public void GoldenInputTestPart1()
+    // {
+    //     var input = File.ReadAllText("../../../input.txt");
+    //     input
+    //         .Split("\r\n")
+    //         .Select(x => CalculateTotalScore(x))
+    //         .Sum()
+    //         .Should().Be(12156);
+    // }
+
+
+    [TestCase(RockThem, DrawIndicator, 4)]
+    [TestCase(PaperThem, DrawIndicator, 5)]
+    [TestCase(ScissorsThem, DrawIndicator, 6)]
     public void DrawsAreTheValueOfTheItemPlayedPlus3(char theirPlay, char ourPlay, int expectedScore)
     {
         CalculateTotalScore($"{theirPlay} {ourPlay}").Should().Be(expectedScore);
     }
 
-    [TestCase(RockThem, ScissorsUs, 3)]
-    [TestCase(PaperThem, RockUs, 1)]
-    [TestCase(ScissorsThem, PaperUs, 2)]
+    [TestCase(RockThem, LoseIndicator, 3)]
+    [TestCase(PaperThem, LoseIndicator, 1)]
+    [TestCase(ScissorsThem, LoseIndicator, 2)]
     public void ALossIsWorthTheItemPlayed(char theirPlay, char ourPlay, int expectedScore)
     {
         CalculateTotalScore($"{theirPlay} {ourPlay}").Should().Be(expectedScore);
     }
 
-    [TestCase(RockThem, PaperUs, 8)]
-    [TestCase(PaperThem, ScissorsUs, 9)]
-    [TestCase(ScissorsThem, RockUs, 7)]
+    [TestCase(RockThem, WinIndicator, 8)]
+    [TestCase(PaperThem, WinIndicator, 9)]
+    [TestCase(ScissorsThem, WinIndicator, 7)]
     public void AWinIsWorthTheItemPlayedPlus6(char theirPlay, char ourPlay, int expectedScore)
     {
         CalculateTotalScore($"{theirPlay} {ourPlay}").Should().Be(expectedScore);
     }
 
     [Test]
-    public void GoldenInputTestPart1()
+    public void GoldenInputTestPart2()
     {
         var input = File.ReadAllText("../../../input.txt");
         input
             .Split("\r\n")
             .Select(x => CalculateTotalScore(x))
             .Sum()
-            .Should().Be(12156);
+            .Should().Be(10835);
     }
 
     private const char RockThem = 'A';
     private const char PaperThem = 'B';
     private const char ScissorsThem = 'C';
-    private const char RockUs = 'X';
-    private const char PaperUs = 'Y';
-    private const char ScissorsUs = 'Z';
+    private const char LoseIndicator = 'X';
+    private const char DrawIndicator = 'Y';
+    private const char WinIndicator = 'Z';
     private const int RockValue = 1;
     private const int PaperValue = 2;
     private const int ScissorsValue = 3;
@@ -64,10 +100,30 @@ public class Day2Tests
 
     private int CalculateTotalScore(string input)
     {
-        if(string.IsNullOrEmpty(input))
+        if (string.IsNullOrEmpty(input))
             return 0;
 
-        return GetValueOfOurPlayedItem(input) + GetWinLossDrawValue(input);
+        RoShamBo theirItem = ToRoShamBo(input.First());
+        RoShamBo ourItem = FigureOutWhatWeShouldPlay(input, theirItem);
+        return GetValueOfOurPlayedItem(ourItem) + GetWinLossDrawValue(theirItem, ourItem);
+    }
+
+    private static RoShamBo FigureOutWhatWeShouldPlay(string input, RoShamBo theirItem)
+    {
+        char winDrawLossIndicator = input.Last();
+        if (winDrawLossIndicator == LoseIndicator)
+        {
+            if(theirItem == RoShamBo.Rock) return RoShamBo.Scissors;
+            if(theirItem == RoShamBo.Paper) return RoShamBo.Rock;
+            if(theirItem == RoShamBo.Scissors) return RoShamBo.Paper;
+        }
+        else if(winDrawLossIndicator == WinIndicator)
+        {
+            if(theirItem == RoShamBo.Rock) return RoShamBo.Paper;
+            if(theirItem == RoShamBo.Paper) return RoShamBo.Scissors;
+            if(theirItem == RoShamBo.Scissors) return RoShamBo.Rock;
+        }
+        return theirItem;
     }
 
     private enum RoShamBo 
@@ -79,24 +135,23 @@ public class Day2Tests
 
     private RoShamBo ToRoShamBo(char item)
     {
-        if(item == RockThem || item == RockUs) return RoShamBo.Rock;
-        if(item == PaperThem || item == PaperUs) return RoShamBo.Paper;
-        if(item == ScissorsThem || item == ScissorsUs) return RoShamBo.Scissors;
+        if(item == RockThem) return RoShamBo.Rock;
+        if(item == PaperThem) return RoShamBo.Paper;
+        if(item == ScissorsThem) return RoShamBo.Scissors;
         return RoShamBo.Rock;
     }
 
-    private int GetWinLossDrawValue(string input)
+    private int GetWinLossDrawValue(RoShamBo theirItem, RoShamBo ourItem)
     {
-        RoShamBo theirItem = ToRoShamBo(input.First());
-        RoShamBo ourItem = ToRoShamBo(input.Last());
-        if (theirItem == ourItem)
-            return DrawValue;
+        if (theirItem == ourItem) return DrawValue;
+
         if (theirItem == RoShamBo.Rock && ourItem == RoShamBo.Scissors)
             return LossValue;
         if(theirItem == RoShamBo.Paper && ourItem == RoShamBo.Rock)
             return LossValue;
         if(theirItem == RoShamBo.Scissors && ourItem == RoShamBo.Paper)
             return LossValue;
+
         if(theirItem == RoShamBo.Rock && ourItem == RoShamBo.Paper)
             return WinValue;
         if(theirItem == RoShamBo.Paper && ourItem == RoShamBo.Scissors)
@@ -106,15 +161,11 @@ public class Day2Tests
         return 0;
     }
 
-    private int GetValueOfOurPlayedItem(string input)
+    private int GetValueOfOurPlayedItem(RoShamBo ourItem)
     {
-        RoShamBo ourItem = ToRoShamBo(input.Last());
-        if(ourItem == RoShamBo.Rock)
-            return RockValue;
-        if(ourItem == RoShamBo.Paper)
-            return PaperValue;
-        if(ourItem == RoShamBo.Scissors)
-            return ScissorsValue;
+        if(ourItem == RoShamBo.Rock) return RockValue;
+        if(ourItem == RoShamBo.Paper) return PaperValue;
+        if(ourItem == RoShamBo.Scissors) return ScissorsValue;
         return 0;
     }
 }
