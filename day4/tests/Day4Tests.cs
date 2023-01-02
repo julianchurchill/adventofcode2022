4b7efdbc7171e587fc9d08aab566209a11074464
Julian Churchill <chewielouie03@gmail.com>
2023-01-02 12:11:51 +0000

    Refactor to push up range calculation out of comparison logic

--- day4/tests/Day4Tests.cs
+++ day4/tests/Day4Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Linq;
 using FluentAssertions;
@@ -46,36 +47,107 @@ public class Day4Tests
             .Should().Be(453);
     }
 
+    [Test]
+    public void NonPartiallyOverlappingRangesMeansZeroRangesContainTheOther()
+    {
+        CountParitallyOverlappingRanges("1-1,2-2").Should().Be(0);
+    }
+
+    [TestCase("1-1,1-1")]
+    [TestCase("11-11,11-11")]
+    public void OneSimplePartlyOverlappingRangeMeansOneRangeContainsTheOther(string input)
+    {
+        CountParitallyOverlappingRanges(input).Should().Be(1);
+    }
+
+    [TestCase("1-3,2-3")]
+    [TestCase("2-3,1-3")]
+    [TestCase("22-24,23-23")]
+    [TestCase("1-3,2-4")]
+    [TestCase("2-4,1-3")]
+    public void OneComplexPartlyOverlappingRangeMeansOneRangeContainsTheOther(string input)
+    {
+        CountParitallyOverlappingRanges(input).Should().Be(1);
+    }
+
+    [TestCase("22-24,31-33")]
+    public void OneComplexNonPartlyOverlappingRangeMeansNoRangeContainsTheOther(string input)
+    {
+        CountParitallyOverlappingRanges(input).Should().Be(0);
+    }
+
+    [Test]
+    public void GoldenInputTestPart2()
+    {
+        var input = File.ReadAllText("../../../input.txt");
+        input.Split("\r\n")
+            .Select(CountParitallyOverlappingRanges)
+            .Sum()
+            .Should().Be(919);
+    }
+
+    private int CountParitallyOverlappingRanges(string input)
+    {
+        var elfPairs = input.Split(",");
+        var firstElf = elfPairs[0];
+        var secondElf = elfPairs[1];
+        if (IsPartlyOverlapping(FindRange(firstElf), FindRange(secondElf)))
+            return 1;
+        return 0;
+    }
+
+    struct Range
+    {
+        public Range(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public int Start;
+        public int End;
+    }
+
+    private static bool IsPartlyOverlapping(Range firstRange, Range secondRange)
+    {
+        if (IsInclusivelyBetween(firstRange.Start, secondRange.Start, secondRange.End) ||
+            IsInclusivelyBetween(firstRange.End, secondRange.Start, secondRange.End))
+            return true;
+        if (IsInclusivelyBetween(secondRange.Start, firstRange.Start, firstRange.End) ||
+            IsInclusivelyBetween(secondRange.End, firstRange.Start, firstRange.End))
+            return true;
+        return false;
+    }
+
     private int CountFullyOverlappingRanges(string input)
     {
         var elfPairs = input.Split(",");
         var firstElf = elfPairs[0];
         var secondElf = elfPairs[1];
-        if (IsFullyOverlapping(firstElf, secondElf))
+        if (IsFullyOverlapping(FindRange(firstElf), FindRange(secondElf)))
             return 1;
         return 0;
     }
 
-    private static bool IsFullyOverlapping(string firstElf, string secondElf)
+    private static bool IsFullyOverlapping(Range firstRange, Range secondRange)
     {
-        var (firstStart, firstEnd) = FindRange(firstElf);
-        var (secondStart, secondEnd) = FindRange(secondElf);
-
-        if (IsInclusivelyBetween(firstStart, secondStart, secondEnd) && IsInclusivelyBetween(firstEnd, secondStart, secondEnd))
+        if (IsInclusivelyBetween(firstRange.Start, secondRange.Start, secondRange.End) &&
+            IsInclusivelyBetween(firstRange.End, secondRange.Start, secondRange.End))
             return true;
-        if (IsInclusivelyBetween(secondStart, firstStart, firstEnd) && IsInclusivelyBetween(secondEnd, firstStart, firstEnd))
+        if (IsInclusivelyBetween(secondRange.Start, firstRange.Start, firstRange.End) &&
+            IsInclusivelyBetween(secondRange.End, firstRange.Start, firstRange.End))
             return true;
         return false;
     }
 
-    private static (int, int) FindRange(string firstElf)
+    private static Range FindRange(string firstElf)
     {
         string[] firstElfSplit = firstElf.Split("-");
         string firstElfStart = firstElfSplit[0];
         string firstElfEnd = firstElfSplit[1];
         var firstStart = int.Parse(firstElfStart);
         var firstEnd = int.Parse(firstElfEnd);
-        return (firstStart, firstEnd);
+        return new Range(firstStart, firstEnd);
     }
 
     private static bool IsInclusivelyBetween(int first, int start, int end)
